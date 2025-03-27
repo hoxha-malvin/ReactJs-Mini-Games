0e09869d9cbf9a9e2230f0575c2a39e1e9984811
hoxha-malvin <hoxhamalvin25@gmail.com>
2025-03-27 19:57:10 +0200

    Finishing Game Over logic with proper messages

--- Capstone-Game/src/App.jsx
+++ Capstone-Game/src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from "react"
 import { languages } from "./languages"
+import { getFarewellText } from "./utils";
 
 export default function AssemblyEndgame() {
     const [currentWord, setCurrentWord] = useState("react");
-
     const [guessedLetters, setGuessedLetters] = useState([]);
     
+
     const wrongGuessedCount = guessedLetters.filter((letter) => !currentWord.includes(letter)).length;
-    console.log(wrongGuessedCount)
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    const isGameLost = wrongGuessedCount > 7;
+    const isGameWon = currentWord.split("").every(letter => guessedLetters.includes(letter));
+    const isGameOver = isGameWon || isGameLost;
+
+    const lastGuessedLetter = guessedLetters[guessedLetters.length - 1];
+    const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter);
+
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+
 
     const languageElements = languages.map((lang, index) => {
       const isLanguageLost = index < wrongGuessedCount;
-
-      const lost = wrongGuessedCount;
-      console.log(lost)
 
       return (
         <span
@@ -47,7 +54,7 @@ export default function AssemblyEndgame() {
       const isCorrect = isGuessed && currentWord.includes(letter);
       const isWrong = isGuessed && !currentWord.includes(letter);
       return (
-        <button onClick={() => addGuessedLetter(letter)} className={` ${isCorrect ? "bg-[#10A95B]" : isWrong ? "bg-[#EC5D49]" : "bg-[#FCBA29]"}
+        <button disabled={isGameOver} aria-disabled={isGameOver} onClick={() => addGuessedLetter(letter)} className={` ${isCorrect ? "bg-[#10A95B]" : isWrong ? "bg-[#EC5D49]" : "bg-[#FCBA29]"}
           h-[35px] w-[35px] border-2 border-[#F9FADA] rounded-[3px]  cursor-pointer text-[#323232]`}
         key={letter}>{letter.toUpperCase()}</button>
       )
@@ -63,9 +70,31 @@ export default function AssemblyEndgame() {
               <p className="text-sm max-w-[350px] mx-auto text-[#8E8E8E]">Guess the word within 8 attempts to keep the
               programming world safe from Assembly!</p>
           </header>
-          <section className="bg-[#10A95B] flex flex-col items-center text-[#F9F4DA] rounded-[4px] my-[30px]">
-              <h2 className="text-xl m-1">You win!</h2>
-              <p className="m-1">Well done! 🎉</p>
+          <section className={`${isGameWon ? 'bg-[#10A95B]' : isGameLost ? 'bg-[#BA2A2A]' : 'bg-[#7A5EA7] border-dashed border-2 border-[#323232] italic'}
+            flex flex-col justify-center items-center text-[#F9F4DA] rounded-[4px] my-[30px] min-h-[60px]`}>
+            {!isGameOver && isLastGuessIncorrect ? (
+                <p className="font-[400] text-center">
+                  {getFarewellText(languages[wrongGuessedCount - 1].name)}
+                </p>
+              ) : isGameOver ? (
+                isGameWon ? (
+                  <>
+                    <h2 className="text-xl m-1">You win!</h2>
+                    <p className="m-1">Well done! 🎉</p>
+                  </>
+                ) : (
+                  <>
+                    <h2 className="text-xl m-1">Game Over!</h2>
+                    <p className="m-1">Better start learning Assembly😭</p>
+                  </>
+                )
+              ) : (
+                <p>Go on!</p>
+              )
+            }
+
+
+              
           </section>
           <section className="flex flex-wrap gap-[5px] justify-center max-w-[350px] mx-auto mb-[36px]">
               {languageElements}
@@ -76,9 +105,12 @@ export default function AssemblyEndgame() {
           <section className="flex flex-wrap gap-[8px] justify-center max-w-[450px] mb-[36px]">
               {keyboardElements}
           </section>
-          <button className="bg-[#11B5E5] rounded-[4px] border-2 border-[#D7D7D7] w-[225px] h-[40px]
-            py-[5px] px-[12px] block mx-[auto] cursor-pointer
-          ">New Game</button>
+          {
+            isGameOver && <button className="bg-[#11B5E5] rounded-[4px] border-2 border-[#D7D7D7] w-[225px] h-[40px]
+              py-[5px] px-[12px] block mx-[auto] cursor-pointer
+              ">New Game</button>
+          }
+          
       </main>
     )
 }
