df0b47af490bba4fae8c65f2f78212dcbcb8cd65
hoxha-malvin <hoxhamalvin25@gmail.com>
2025-03-27 17:58:15 +0200

    Creating the interface

--- Capstone-Game/src/App.jsx
+++ Capstone-Game/src/App.jsx
@@ -2,59 +2,83 @@ import { useState } from "react"
 import { languages } from "./languages"
 
 export default function AssemblyEndgame() {
-    const [currentWord, setCurrentWord] = useState("react")
+    const [currentWord, setCurrentWord] = useState("react");
+
+    const [guessedLetters, setGuessedLetters] = useState([]);
     
+    const wrongGuessedCount = guessedLetters.filter((letter) => !currentWord.includes(letter)).length;
+    console.log(wrongGuessedCount)
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    const languageElements = languages.map(lang => {
-        const styles = {
+    const languageElements = languages.map((lang, index) => {
+      const isLanguageLost = index < wrongGuessedCount;
+
+      const lost = wrongGuessedCount;
+      console.log(lost)
+
+      return (
+        <span
+          className={`rounded-[3px] p-1 relative ${isLanguageLost ? 'lost' : ''}`}
+          style={{
             backgroundColor: lang.backgroundColor,
-            color: lang.color
-        }
-        return (
-            <span
-                className="rounded-[3px] p-1"
-                style={styles}
-                key={lang.name}
-            >
-                {lang.name}
-            </span>
-        )
-    })
-    
+            color: lang.color,
+            position: "relative",
+            display: "inline-block",
+          }}
+          key={lang.name}
+        >
+          {lang.name}
+
+          
+        </span>
+      )
+    });
+    const addGuessedLetter = (letter) => {
+      setGuessedLetters(prevLetters => prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter]);
+    }
+
     const letterElements = currentWord.split("").map((letter, index) => (
         <span className="h-[40px] w-[40px] bg-[#323232] flex justify-center items-center text-2xl border-b-1
-        " key={index}>{letter.toUpperCase()}</span>
-    ))
+        " key={index}>{guessedLetters.includes(letter) ? letter.toUpperCase() : ''}</span>
+    ));
     
-    const keyboardElements = alphabet.split("").map(letter => (
-        <button className="h-[35px] w-[35px] bg-[#FCBA29] border-2 border-[#F9FADA] rounded-[3px]  cursor-pointer text-[#323232]
-        " key={letter}>{letter.toUpperCase()}</button>
-    ))
+    const keyboardElements = alphabet.split("").map(letter => {
+      const isGuessed = guessedLetters.includes(letter);
+      const isCorrect = isGuessed && currentWord.includes(letter);
+      const isWrong = isGuessed && !currentWord.includes(letter);
+      return (
+        <button onClick={() => addGuessedLetter(letter)} className={` ${isCorrect ? "bg-[#10A95B]" : isWrong ? "bg-[#EC5D49]" : "bg-[#FCBA29]"}
+          h-[35px] w-[35px] border-2 border-[#F9FADA] rounded-[3px]  cursor-pointer text-[#323232]`}
+        key={letter}>{letter.toUpperCase()}</button>
+      )
+        
+    });
+
+    
 
     return (
-        <main>
-            <header className="text-center">
-                <h1 className="text-2xl font-semibold text-[#F9F4DA]">Assembly: Endgame</h1>
-                <p className="text-sm max-w-[350px] mx-auto text-[#8E8E8E]">Guess the word within 8 attempts to keep the
-                programming world safe from Assembly!</p>
-            </header>
-            <section className="bg-[#10A95B] flex flex-col items-center text-[#F9F4DA] rounded-[4px] my-[30px]">
-                <h2 className="text-xl m-1">You win!</h2>
-                <p className="m-1">Well done! 🎉</p>
-            </section>
-            <section className="flex flex-wrap gap-[5px] justify-center max-w-[350px] my-auto mb-[36px]">
-                {languageElements}
-            </section>
-            <section className="flex justify-center gap-[2px] mb-[20px]">
-                {letterElements}
-            </section>
-            <section className="flex flex-wrap gap-[8px] justify-center max-w-[450px] mb-[36px]">
-                {keyboardElements}
-            </section>
-            <button className="bg-[#11B5E5] rounded-[4px] border-2 border-[#D7D7D7] w-[225px] h-[40px]
-              py-[5px] px-[12px] block mx-[auto] cursor-pointer
-            ">New Game</button>
-        </main>
+      <main>
+          <header className="text-center">
+              <h1 className="text-2xl font-semibold text-[#F9F4DA]">Assembly: Endgame</h1>
+              <p className="text-sm max-w-[350px] mx-auto text-[#8E8E8E]">Guess the word within 8 attempts to keep the
+              programming world safe from Assembly!</p>
+          </header>
+          <section className="bg-[#10A95B] flex flex-col items-center text-[#F9F4DA] rounded-[4px] my-[30px]">
+              <h2 className="text-xl m-1">You win!</h2>
+              <p className="m-1">Well done! 🎉</p>
+          </section>
+          <section className="flex flex-wrap gap-[5px] justify-center max-w-[350px] mx-auto mb-[36px]">
+              {languageElements}
+          </section>
+          <section className="flex justify-center gap-[2px] mb-[20px]">
+              {letterElements}
+          </section>
+          <section className="flex flex-wrap gap-[8px] justify-center max-w-[450px] mb-[36px]">
+              {keyboardElements}
+          </section>
+          <button className="bg-[#11B5E5] rounded-[4px] border-2 border-[#D7D7D7] w-[225px] h-[40px]
+            py-[5px] px-[12px] block mx-[auto] cursor-pointer
+          ">New Game</button>
+      </main>
     )
 }
