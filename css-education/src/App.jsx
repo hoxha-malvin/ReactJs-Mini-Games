d5e65a74f323c915fbb7a8257d5afca15635125e
hoxha-malvin <hoxhamalvin25@gmail.com>
2025-04-04 21:58:13 +0300

    Setting all the files for the education

---
+++ css-education/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <main className="max-w-full max-h-full">
+      <aside>
+        1
+      </aside>
+
+      <aside>
+        2
+      </aside>
+    </main>
+  )
+}
+
+export default App
---
+++ css-education/src/tasks.js
@@ -0,0 +1,9 @@
+const tasks = [
+    {
+        id: 1,
+        description: "Move the frog to the center.",
+        answer: "justify-content: center",
+        targetPosition: [1, 3],
+        properties: "flex items-center"
+    }
+]
