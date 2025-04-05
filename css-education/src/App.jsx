61bebc89d15096b183a2bef1015404947b31da49
hoxha-malvin <hoxhamalvin25@gmail.com>
2025-04-05 14:14:01 +0300

    Creating the second session and UI

--- css-education/src/App.jsx
+++ css-education/src/App.jsx
@@ -1,6 +1,6 @@
 import tasks from './tasks'
-import { useState } from 'react';
-import atronaut from './assets/atronaut.svg';
+import { use, useState } from 'react';
+import astronaut from './assets/astronaut.svg';
 import flag from './assets/flag.svg';
 
 const matchCSS = (input, expected) => {
@@ -9,18 +9,46 @@ const matchCSS = (input, expected) => {
   return pattern.test(input.trim());
 };
 
+
+
 const App = () => {
-  const [userInput, setUserInput] = useState("");
-  const currentTask = tasks[0];
+  const [userInputs, setUserInputs] = useState(() => tasks.map(() => ""));
+
+  
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentTask = tasks[currentIndex];
+
+  const handleInputChange = (value) => {
+    setUserInputs(prevInput => {
+      const newInputs = [...prevInput];
+      newInputs[currentIndex] = value;
+      return newInputs;
+    })
+  }
+
+  const userInput = userInputs[currentIndex];
   
   const isCorrect = matchCSS(userInput, currentTask.answer);
   const correctAnswer = isCorrect ? currentTask.properties : "";
   
+  
+
+  const changePage = (direction) => {
+    setCurrentIndex(prevIndex  => {
+      if (direction === 'next') {
+        return prevIndex < tasks.length - 1 ? prevIndex + 1 : prevIndex;
+      } else if (direction === 'previous'){
+        return prevIndex > 0 ? prevIndex - 1 : prevIndex;
+      }
+      return prevIndex;
+    })
+  }
 
   return (
-    <main className="flex w-full min-h-screen bg-gradient-to-br from-blue-100 to-blue-300 font-sans">
+    <main className="flex max-xl:flex-col w-full min-h-screen bg-gradient-to-br from-blue-100 to-blue-300 font-sans">
     {/* Left Side */}
-    <aside className="w-full flex-1 flex flex-col p-8 space-y-6 bg-white shadow-xl rounded-r-4xl">
+    <aside className="w-full flex-1 flex flex-col p-8 space-y-6 bg-white shadow-xl xl:rounded-r-4xl ">
       <h1 className="text-4xl font-bold text-blue-700">Flexbox Atronaut</h1>
       
       <p className="text-lg text-gray-700 leading-relaxed flex-1">
@@ -37,21 +65,22 @@ const App = () => {
         For example, <code className="bg-blue-100 text-blue-800 px-1 py-0.5 rounded">justify-content: flex-end;</code> will move the frog to the right.
       </p>
   
-      <div className="bg-gray-100 p-4 rounded-xl ">
-        <Compiler userInput={userInput} setUserInput={setUserInput} />
+      <div className="bg-gray-100 p-4 rounded-xl flex-1">
+        <Compiler userInput={userInput} setUserInput={handleInputChange} changePage={changePage} isCorrect={isCorrect}/>
       </div>
+
     </aside>
   
     {/* Right Side */}
     <aside className="w-full flex-1 flex items-center justify-center p-10">
-      <div className="relative bg-white/30 backdrop-blur-md border border-white/40 shadow-2xl rounded-3xl p-10 w-full max-w-3xl h-[70vh] flex items-center justify-center transition-all duration-300 ease-in-out">
+      <div className="relative bg-white/30 backdrop-blur-md border border-white/40 shadow-2xl rounded-3xl sm:p-10 w-full max-w-3xl h-[70vh] flex items-center justify-center transition-all duration-300 ease-in-out">
         {/* Frog + Effects Container */}
         <div className={`w-full h-full flex items-center  relative transition-all duration-300 ease-in-out ${correctAnswer}`}>
           {/* Main character */}
-          <img src={atronaut} alt="atronaut" className={`${isCorrect ? 'mr-20' : ''} z-10`} width={100} />
+          <img src={astronaut} alt="atronaut" className='z-10' width={100} />
 
           {/* Background effects */}
-          <div className="absolute w-full flex justify-center items-center top-1/2 transform -translate-y-1/2">
+          <div className={currentTask.flag}>
               <img src={flag} alt="flag" className="z-0 scale-90 transition-transform duration-300" width={100} />
           </div>
         </div>
@@ -63,8 +92,8 @@ const App = () => {
   )
 }
 
-function Compiler({userInput, setUserInput}) {
-  const task = tasks[0];
+function Compiler({userInput, setUserInput, changePage, isCorrect}) {
+  const task = tasks[1];
   const contents = task.content.split(";").filter(line => line.trim() !== "");
   const lines = Array.from({ length: task.lines });
 
@@ -94,8 +123,29 @@ function Compiler({userInput, setUserInput}) {
           />
         </div>
         <span>{"}"}</span>
+        <div className='mt-10 flex gap-2'>
+        <button 
+            onClick={() => changePage('previous')} 
+            className='px-2 py-2 rounded-lg text-white bg-gray-700 hover:bg-gray-800 transition duration-300 ease-in-out'
+          >
+            Previous
+          </button>
+
+          <button 
+            disabled={!isCorrect} 
+            onClick={() => changePage('next')} 
+            className={`px-2 py-2 rounded-lg text-white font-semibold 
+              ${!isCorrect ? 'bg-blue-100 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'} 
+              transition duration-300 ease-in-out`}
+          >
+            Next
+          </button>
+          
+        </div>
+
         
       </div>
+      
     </div>
   );
 }
--- css-education/src/tasks.js
+++ css-education/src/tasks.js
@@ -1,11 +1,21 @@
 const tasks = [
     {
-        id: 1,
+        id: 0,
         description: "Move the frog to the center.",
         answer: "justify-content: center",
         lines: 5,
         properties: 'justify-center',
-        content: 'display: flex; align-items: center;'
+        content: 'display: flex; align-items: center;',
+        flag: 'absolute w-full flex justify-center items-center top-1/2 transform -translate-y-1/2'
+    },
+    {
+        id: 1,
+        description: "Move the frog to the end.",
+        answer: "justify-content: end",
+        lines: 5,
+        properties: 'justify-end',
+        content: 'display: flex; align-items: center;',
+        flag: 'absolute w-full flex justify-end items-center top-1/2 transform -translate-y-1/2'
     }
 ];
 
