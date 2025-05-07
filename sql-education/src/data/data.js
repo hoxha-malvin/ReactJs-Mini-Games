3cfa2c87a6f1312c0e56bd74200049e864561d9d
hoxha-malvin <hoxhamalvin25@gmail.com>
2025-05-07 11:07:13 +0300

    Testing outro dialogues

--- sql-education/src/data/data.js
+++ sql-education/src/data/data.js
@@ -17,7 +17,7 @@ SELECT column_name FROM table_name WHERE column_name < value;`,
       { name: "Yaddle" }
     ]
   },
-  /*{
+  {
     id: 2,
     hint: "Masters, 40 years or more, they must be.",
     table: "padawans",
@@ -198,7 +198,7 @@ SELECT SUM(column_name) FROM table_name;`,
     resulted_table: [
       { total_age: 987 }
     ]
-  }*/
+  }
 ];
 
 
